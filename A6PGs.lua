4df872f8f0a8451d5d043cdda51f87c2f7d938df
aux0on <lzzxhq@gmail.com>
2025-09-10 09:23:43 -0700

    Update A6PGs.lua

--- A6PGs.lua
+++ A6PGs.lua
@@ -1172,7 +1172,7 @@ do
             ReplicatedStorage:WaitForChild("Remotes"):WaitForChild("Extras"):WaitForChild("ReplicateToy"):InvokeServer(unpack(args))
         end)
 
-        my_own_section:AddLabel('Credits: <font color="rgb(0,255,0)">@not_.</font>', nil, true)
+        my_own_section:AddLabel('Credits: <font color="rgb(0,255,0)">@not_.gato</font>', nil, true)
     else
         warn("Failed to create Spray Paint section! Plugin not loaded.")
     end
