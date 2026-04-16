8f83d18fe16f07a252b454166d3063b5d3602c47
lzzzx <lzzxhq@gmail.com>
2026-04-15 18:24:36 -0700

    Update A6PGs.lua

--- A6PGs.lua
+++ A6PGs.lua
@@ -3940,6 +3940,9 @@ end)
 
 fps_ping_section:AddParagraph("Skidded & Improved By:", "@lzzzx")
 
+local creditsSection = shared.AddSection("Credits")
+creditsSection:AddParagraph("@lzzzx", "Made this plugin, if you have requests feel free to ask.") end
+
 RootMaid:GiveTask(function()
     
 end)
